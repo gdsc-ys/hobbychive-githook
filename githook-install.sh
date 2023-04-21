b471370694fdfb2bb46a6dd2f6fb019a5283c100
hobbychive <parksw123098@gmail.com>
2023-04-21 16:54:17 +0900

    githook install

--- githook-install.sh
+++ githook-install.sh
@@ -1,2 +1,2 @@
-ln -s ./hook/pre-commit.sh ./.git/hooks/pre-commit
-ln -s ./hook/pre-push.sh ./.git/hooks/pre-push
+ln -s ./hook/pre-commit.sh ./.git/pre-commit
+ln -s ./hook/pre-push.sh ./.git/pre-push
